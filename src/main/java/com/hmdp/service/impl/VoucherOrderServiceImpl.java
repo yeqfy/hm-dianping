c63a1060e42b55c934c311906a7347f75135d559
Sea-sheep <2745607866@qq.com>
2026-07-09 16:31:47 +0800

    优惠券秒杀功能的实现

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.Voucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +29,72 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker idWorker;
+
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        // 1. 查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        // 2. 判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始");
+        }
+        // 3. 判断秒杀是否结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已结束");
+        }
+        // 4. 判断库存是否为空
+        if (voucher.getStock()<1) {
+            return Result.fail("库存不足");
+        }
+
+        // 悲观锁实现一人一单
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象（事务）
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+    }
+
+    @Transactional
+    public Result createVoucherOrder(Long voucherId) {
+        // 5.实现一人一单
+        Long userId = UserHolder.getUser().getId();
+        // 5.1. 查询用户id，订单id
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        // 5.2. 判断是否存在
+        if (count>0) {
+            return Result.fail("用户已经购买过一次了！");
+        }
+        // 6. 扣减库存
+        Boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1")  // set stock = ?
+                .eq("voucher_id", voucherId)  // where voucher_id = ?
+                .gt("stock", 0) // CAS 乐观锁 解决超卖问题        and stock > 0
+                .update();
+        if (!success) {
+            return Result.fail("库存不足");
+        }
+        // 7. 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 7.1. 订单id
+        Long orderId = idWorker.nextId("order");
+        voucherOrder.setId(orderId);
+        // 7.2. 用户id
+        //Long userId = UserHolder.getUser().getId();
+        voucherOrder.setUserId(userId);
+        // 7.3. 优惠券id
+        voucherOrder.setVoucherId(voucherId);
+
+        // 8. 保存订单到数据库
+        save(voucherOrder);
+        // 9. 返回订单id
+        return Result.ok(orderId);
+    }
 }
